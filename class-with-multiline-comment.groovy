7a4e6ecb974e868b1477b49e6d189c3b413a60c3
Gustavo Andrade <gustavoandrade82@gmail.com>
2016-01-16 16:35:38 -0200

    Having some experience with Groovy

---
+++ class-with-multiline-comment.groovy
@@ -0,0 +1,16 @@
+/** 
+ * A class description
+ * /
+class Person {
+
+	String name;
+
+	/** 
+	 * Creates a greeting method for a certain person.
+	 * @param otherPerson the person to greet
+	 * @return a greeting message
+	 * /
+	String greet(String otherPerson) {
+		'Hello ${otherPerson}'
+	}
+}
---
+++ multiline-comment.groovy
@@ -0,0 +1,4 @@
+/* a standalone multiline comment
+   spanning two lines */
+print 'hello'
+print 1 /* one */ + 2 /* two */
